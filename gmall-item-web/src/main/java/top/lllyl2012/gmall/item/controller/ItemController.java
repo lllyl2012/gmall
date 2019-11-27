0cf3531b879c8551fba7b0886799cd6bc69a7114
lllyl2012 <qibuqimingzi@163.com>
2019-11-27 22:04:41 +0800

    thymeleaf

--- gmall-item-web/src/main/java/top/lllyl2012/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/top/lllyl2012/gmall/item/controller/ItemController.java
@@ -1,6 +1,7 @@
 package top.lllyl2012.gmall.item.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ItemController {
 
     @RequestMapping("/test")
-    public String test() {
+    public String test(Model model) {
+        model.addAttribute("index", "hello");
         return "index";
     }
 }
