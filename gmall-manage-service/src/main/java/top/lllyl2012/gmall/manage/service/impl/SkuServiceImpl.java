5121304847aedaab48f3bcc86faf5a5c14ecf400
lllyl2012 <229707363@qq.com>
2019-12-07 21:45:34 +0800

    引入redis

--- gmall-manage-service/src/main/java/top/lllyl2012/gmall/manage/service/impl/SkuServiceImpl.java
+++ gmall-manage-service/src/main/java/top/lllyl2012/gmall/manage/service/impl/SkuServiceImpl.java
@@ -1,7 +1,10 @@
 package top.lllyl2012.gmall.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 import top.lllyl2012.gmall.bean.PmsSkuImage;
 import top.lllyl2012.gmall.bean.PmsSkuInfo;
 import top.lllyl2012.gmall.manage.mapper.PmsSkuAttrValueMapper;
@@ -9,10 +12,12 @@ import top.lllyl2012.gmall.manage.mapper.PmsSkuImageMapper;
 import top.lllyl2012.gmall.manage.mapper.PmsSkuInfoMapper;
 import top.lllyl2012.gmall.manage.mapper.PmsSkuSaleAttrValueMapper;
 import top.lllyl2012.gmall.service.SkuService;
+import top.lllyl2012.gmall.utils.RedisUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class SkuServiceImpl implements SkuService {
@@ -28,6 +33,9 @@ public class SkuServiceImpl implements SkuService {
 
     @Autowired
     private PmsSkuSaleAttrValueMapper pmsSkuSaleAttrValueMapper;
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Override
     public void skuService(PmsSkuInfo pmsSkuInfo) {
@@ -65,8 +73,39 @@ public class SkuServiceImpl implements SkuService {
 
     @Override
     public PmsSkuInfo getSkuById(String skuId) {
-return null;
+        PmsSkuInfo pmsSkuInfo;
+        Jedis jedis = redisUtil.getRedis();
 
+        //查询缓存
+        String skuKey = "sku:" + skuId + ":info";
+        String skuJson = jedis.get(skuKey);
+        if (StringUtils.isNotEmpty(skuJson)) {
+            pmsSkuInfo = JSON.parseObject(skuJson, PmsSkuInfo.class);
+        }else {
+            String lockKey = "sku:" + skuId + ":lock";
+            //设置分布式锁
+            String uuid = UUID.randomUUID().toString();
+            String ok = jedis.set(lockKey, uuid, "nx", "px", 10);
+            if (ok.equals("OK")) {
+                pmsSkuInfo = getSkuByIdFromDB(skuId);
+                if (pmsSkuInfo != null) {
+                    jedis.set(skuKey, JSON.toJSONString(pmsSkuInfo));
+                }else{
+                    //为了防止缓存穿透,null值设置给redis
+                    jedis.setex(skuKey,60*5, JSON.toJSONString(""));
+                }
+                String lockToken = jedis.get(lockKey);
+                if (lockToken.equals(uuid)) {
+                    jedis.del(lockKey);
+                }
+            }else{
+                //设置失败，自旋（在该线程睡眠一段时间后重新反问该方法）
+                pmsSkuInfo = getSkuById(skuId);
+            }
+
+        }
+        jedis.close();
+        return pmsSkuInfo;
     }
 
     @Override
